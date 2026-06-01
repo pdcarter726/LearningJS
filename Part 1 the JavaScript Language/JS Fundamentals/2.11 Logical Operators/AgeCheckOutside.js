add73d0157489ff83c0308f5b79a6d657ae71152
pdcarter726 <pdcarter726@hotmail.com>
2026-06-01 11:47:43 -0400

    Tasks from 2.11 Logical Operators

---
+++ Part 1 the JavaScript Language/JS Fundamentals/2.11 Logical Operators/AgeCheckOutside.js
@@ -0,0 +1,10 @@
+/*
+    Write an if condition to check that age
+    is not between 14 and 90 inclusively
+*/
+
+// First variant using !
+if(!(age >= 14 && age <= 90))
+
+// Second variant without !
+if (age < 14 || age > 90)
