8ccf2fcf628ecae63651e5747b21604c2f4782df
pdcarter726 <pdcarter726@hotmail.com>
2026-06-01 15:43:51 -0400

    Task from 2.16 Arrow Functions

---
+++ Part 1 the JavaScript Language/JS Fundamentals/2.17 Arrow Functions/RewriteWithArrows.js
@@ -0,0 +1,26 @@
+/*
+    Rewrite the following function expressions using arrow notation
+
+    function ask(question, yes, no) {
+         if (confirm(question)) yes();
+        else no();
+    }   
+
+    ask(
+        "Do you agree?",
+        function() { alert("You agreed."); },
+        function() { alert("You canceled the execution."); }
+    );
+*/
+
+function ask(question, yes, no) {
+    if (confirm(question)) yes();
+    else no();
+}   
+
+ask(
+    "Do you agree?"
+    () => alert("You agreed."),
+    () => alert("You canceled the execution.")
+);
+
